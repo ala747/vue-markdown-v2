72b6f1d9a48c37c4a2af86747a1c32c9ab0eb998
Akirami <akira4210@gmail.com>
2019-05-16 12:52:06 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,39 @@
 var webpack = require('webpack');
 var version = require("./package.json").version;
+var path = require('path');
 var banner =
   "/**\n" +
-  " * vue-markdown v" + version + "\n" +
-  " * https://github.com/miaolz123/vue-markdown\n" +
+  " * vue-markdown v2" +
+  " * https://github.com/Akirami/vue-markdown-v2\n" +
   " * MIT License\n" +
   " */\n";
 
 module.exports = {
+  mode: 'development',
   entry: './src/build.js',
   output: {
-    path: './dist',
+    path: path.join(__dirname,"./dist"),
     filename: 'vue-markdown.js',
     library: 'VueMarkdown',
     libraryTarget: 'umd'
   },
   plugins: [
-    new webpack.BannerPlugin(banner, { raw: true })
+    new webpack.BannerPlugin(banner)
   ],
   module: {
-    loaders: [{
+    rules: [{
       test: /\.vue$/,
-      loader: 'vue'
+      loader: "vue-loader"
     }, {
-        test: /\.css$/,
-        loader: "style!css"
-      }, {
         test: /\.js$/,
-        loader: 'babel',
+        loader: "babel-loader",
         exclude: /node_modules/
       }, {
+        test: /\.css$/,
+        loader: ['vue-style-loader', 'css-loader']
+      }, {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: "json-loader"
       }]
   },
 }
